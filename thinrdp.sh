1ee7bce24e17382a2be684243e48488ce4cb610c
RIOPARAISO <evandro@rioparaiso.com.br>
2024-05-22 13:29:59 -0300

    Update thinrdp.sh

--- thinrdp.sh
+++ thinrdp.sh
@@ -5,7 +5,6 @@
 
 apt update
 apt install xserver-xorg yad xinit lightdm-remote-session-freerdp2 gpm ntpdate
-wget  https://raw.githubusercontent.com/RIOPARAISO/thinlient/main/.xinitrc?token=GHSAT0AAAAAACSUQPVBTIVAF4ZBUL4QSMACZSOB24Q
-mv .xinitrc\?token\=GHSAT0AAAAAACSUQPVBTIVAF4ZBUL4QSMACZSOB24Q /home/pi/.xinitrc
+https://raw.githubusercontent.com/RIOPARAISO/thinlient/main/.xinitrc
 echo sudo startx  >> /home/pi/.bashrc
 echo exit >> /home/pi/.bashrc
