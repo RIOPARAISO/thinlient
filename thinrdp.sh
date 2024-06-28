f323330ee90e7957d8356f7b4b7b714d30e4f522
RIOPARAISO <evandro@rioparaiso.com.br>
2024-06-28 13:29:48 -0300

    Update thinrdp.sh

--- thinrdp.sh
+++ thinrdp.sh
@@ -8,3 +8,6 @@ wget https://raw.githubusercontent.com/RIOPARAISO/thinlient/main/.xinitrc
 chmod +x /home/pi/.xinitrc
 echo startx  >> /home/pi/.bashrc
 echo exit >> /home/pi/.bashrc
+
+sudo adduser -m -s /bin/bash --home /home/adminsys adminsys 
+password adminsys
